17573dcd148a6b006e99cf94dfba18a2cf2e971c
ys <oneyul0313@naver.com>
2022-06-14 09:59:35 +0900

    로그인 완료

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {useNavigate} from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
-
+// import {useDispatch} from 'react-redux'
+// import {loginUser} from "../redux/modules/User.js";
 
 function LogIn() {
-
+    // const dispatch=useDispatch();
     const navigate = useNavigate();
     const id_ref = React.useRef(null);
     const pw_ref = React.useRef(null);
@@ -13,23 +14,46 @@ function LogIn() {
     const [username, setUserName] = React.useState("");
     const [password, setPwd] = React.useState("");
 
-    const login = ()=> {
-       axios.post('/login',{
-            "ID": id_ref.current.value,
-            "password": pw_ref.current.value}, {
+    // const onSubmitHandler = ()=> {
 
-            }).then(function(response){
-                alert("로그인이 완료되었습니다!")
-                navigate('/');
-                
+    //     .preventDefault();
+        
+    //     // console.log("username", username);
+    //     // console.log("password", password);
+    //     let body = {
+    //         "userId": id_ref.current.value,
+    //         "password": pw_ref.current.value
+    //     }
 
-                console(response)
-            }).catch(function(error){
-                alert("로그인 정보가 잘못되었습니다!")
-            })
+    //     dispatch(loginUser(body))
+    //     .then(response => {
+    //         if(response.payload.loginSuccess){
+              
+    //          navigate('/');
+    //         }else{
+    //             alert("로그인 정보가 잘못되었습니다!")
+    //         }
+    //     })
+      
+    // }
   
+ 
+    const loginAxios= ()=> {
+        axios.post('http:// /api/user/login', {
+            "userId": id_ref.current.value,
+            "password": pw_ref.current.value
+        }).then(function(response)  {
+            alert("로그인 되었습니다!")
+            navigate('/');
+
+            localStorage.setItem('access_token', response.data.token);
+
+            console.log(response)
+        }).catch(function(error){
+            alert("로그인정보가 틀렸습니다.")
+        })
     }
-  
+
  
     return (
         <div>
@@ -37,15 +61,15 @@ function LogIn() {
     <Container>
     
     
-    <div >
+    <div> 
             <input   placeholder="아이디를 입력해주세요." 
             onChange={(e) => setUserName(e.target.value)} ref={id_ref}/>    <br/>
             <input type="Password"   placeholder="비밀번호를 입력해주세요." 
             onChange={(e) => setPwd(e.target.value)} ref={pw_ref}/>   <br/>      
       
           
-            <Btn1 onClick={login} type="submit"> 로그인  </Btn1>
-        
+            <Btn1 onClick={loginAxios}> 로그인  </Btn1>
+            
             <Btn2 onClick={()=>{navigate('/SignUp')}} > 회원가입</Btn2>
           </div>
     
